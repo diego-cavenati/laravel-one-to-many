d7a7a564eb6dee760a08b57b43ee799b6771a064
diego-cavenati <diegocavenati@gmail.com>
2023-01-16 16:56:23 +0100

    Define relationship inside each model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'slug', 'vote', 'link', 'cover_image'];
+    protected $fillable = ['title', 'description', 'slug', 'vote', 'link', 'cover_image', 'type_id'];
+
+    /**
+     * Get the category that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
